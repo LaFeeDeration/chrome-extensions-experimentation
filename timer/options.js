ca8b03b3c52699739e8a94a06d4eb08f9563fa93
Alexandre ALLARD <strongbow.alex@gmail.com>
2023-03-20 15:57:57 +0100

    Initial commit

---
+++ timer/options.js
@@ -0,0 +1,17 @@
+const nameInput = document.getElementById("name-input");
+const saveBtn = document.getElementById("save-btn");
+const timeInput = document.getElementById("time-input");
+
+saveBtn.addEventListener("click", () => {
+    const name= nameInput.value;
+    const notificationTime = timeInput.value
+    chrome.storage.sync.set({
+        name: name,
+        notificationTime: notificationTime,
+    })
+})
+
+chrome.storage.sync.get(["name", "notificationTime"], (res) => {
+    nameInput.value = res.name ?? "???"
+    timeInput.value = res.notificationTime ?? 1000
+})
